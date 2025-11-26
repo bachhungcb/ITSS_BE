adf868527d22327fa67d5a344131e9fa26d56611
bachhungcb <bachhungcb@gmail.com>
2025-11-26 16:06:08 +0700

    da chay duoc

--- Application/Features/GymFeatures/Queries/SearchGymAdvancedQuery.cs
+++ Application/Features/GymFeatures/Queries/SearchGymAdvancedQuery.cs
@@ -15,7 +15,8 @@ public record SearchGymsAdvancedQuery(string Address, string? Keyword, string? S
             _gymSearchService = gymSearchService;
         }
 
-        public async Task<IEnumerable<GymPlace>> Handle(SearchGymsAdvancedQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<GymPlace>> Handle(SearchGymsAdvancedQuery request,
+            CancellationToken cancellationToken)
         {
             // 1. Lấy dữ liệu thô từ OSM (đã có tính distance ở Bước 2)
             var gyms = await _gymSearchService.SearchGymsAroundAddressAsync(request.Address);
@@ -24,14 +25,19 @@ public record SearchGymsAdvancedQuery(string Address, string? Keyword, string? S
             if (!string.IsNullOrWhiteSpace(request.Keyword))
             {
                 var keyword = request.Keyword.ToLower();
-                gyms = gyms.Where(g => 
+                gyms = gyms.Where(g =>
                     (g.Name != null && g.Name.ToLower().Contains(keyword)) ||
-                    (g.Address != null && g.Address.ToLower().Contains(keyword))
+                    (g.Address != null && g.Address.ToLower().Contains(keyword)) ||
+                    (g.Sports.Contains(keyword))
                 ).ToList();
             }
 
             // 3. Sắp xếp (Sort)
             if (request.SortBy == "distance_asc") // Gần nhất
+            {
+                gyms = gyms.OrderBy(g => g.DistanceInMeters).ToList();
+            }
+            else
             {
                 gyms = gyms.OrderBy(g => g.DistanceInMeters).ToList();
             }
